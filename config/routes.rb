f8124595cb6c428b9574be209750a94bb24c0356
Saraswati Saud <saraswati.saud@gmail.com>
2017-01-19 05:59:33 +0545

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
   root 'static_pages#home'
-#  get 'users/index'
-#  get 'users/new'
-#  get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-#  post '/logout', to: 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-
+  resources :account_activations, only: [:edit]
 end
